c3903d16474d02f8e279a81ad8168384dfaff2f9
rdoliveira <rdeoliveira@gencovery.com>
2021-12-15 15:15:07 +0000

    (add) uBiome: output files front view

--- src/gws_ubiome/file/qiime2_folder.py
+++ src/gws_ubiome/file/qiime2_folder.py
@@ -158,21 +158,284 @@ class Qiime2RarefactionFolder(Folder):
 class Qiime2TaxonomyDiversityFolder(Folder):
     ''' Qiime2TaxonomyDiversityFolder Folder file class '''
 
+    bray_curtis_table_path: str = StrRField()
+    chao_1_table_path: str = StrRField()
+    evenness_table_path: str = StrRField()
+    faith_pd_table_path: str = StrRField()
+    feature_freq_detail_table_path: str = StrRField()
+    inv_simpson_table_path: str = StrRField()
+    jaccard_distance_table_path: str = StrRField()
+    jaccard_unweighted_unifrac_distance_table_path: str = StrRField()
+    taxo_level_1_table_path: str = StrRField()
+    taxo_level_2_table_path: str = StrRField()
+    taxo_level_3_table_path: str = StrRField()
+    taxo_level_4_table_path: str = StrRField()
+    taxo_level_5_table_path: str = StrRField()
+    taxo_level_6_table_path: str = StrRField()
+    taxo_level_7_table_path: str = StrRField()
+    observed_features_vector_table_path: str = StrRField()
+    shannon_vector_table_path: str = StrRField()
+    simpson_table_path: str = StrRField()
+    unweighted_unifrac_distance_table_path: str = StrRField()
+    weighted_unifrac_distance_table_path: str = StrRField()
+
     @view(view_type=TableView, 
-            human_name='XXXIndexTable', 
-            short_description='Table view of the XXX index table'
+            human_name='BrayCurtisIndexTable', 
+            short_description='Table view of the Bray-Curtis index table'
     )
-    def view_as_table_observed_features(self, params: ConfigParams) -> TableView:
+    def view_as_table_bray_curtis(self, params: ConfigParams) -> TableView:
         table: Table = Table.import_from_path(
-            file=File(path=self.observed_features_table_path),
+            file=File(path=self.bray_curtis_table_path),
             params = ConfigParams({})
         )
 
         return TableView(data=table.get_data())  
 
 
+    @view(view_type=TableView, 
+            human_name='chao1IndexTable', 
+            short_description='Table view of the chao1 index table'
+    )
+    def view_as_table_chao_1(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.chao_1_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='EvennessIndexTable', 
+            short_description='Table view of the evenness index table'
+    )
+    def view_as_table_evenness(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.evenness_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='FaithPdIndexTable', 
+            short_description='Table view of the faith_pd index table'
+    )
+    def view_as_table_faith_pd(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.faith_pd_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='FeatureFreqDetailIndexTable', 
+            short_description='Table view of the feature frequrncies detail  table'
+    )
+    def view_as_table_features_freq(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.feature_freq_detail_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='InvSimpsonIndexTable', 
+            short_description='Table view of the inverse simpson (i.e, 1/simpson index) index table'
+    )
+    def view_as_table_inv_simpson(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.inv_simpson_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='JaccardDistanceIndexTable', 
+            short_description='Table view of the jaccard distance index table'
+    )
+    def view_as_table_jaccard_distance(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.jaccard_distance_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='JaccardUnweightedDistanceIndexTable', 
+            short_description='Table view of the jaccard distance (unweighted unifrac version) index table'
+    )
+    def view_as_table_jaccard_unweighted_unifrac_distance(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.jaccard_unweighted_unifrac_distance_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='TaxoLevel1Table', 
+            short_description='Table view of the taxonomic level 1 (i.e, Kingdom) table'
+    )
+    def view_as_table_taxo_level_1(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.taxo_level_1_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='TaxoLevel2Table', 
+            short_description='Table view of the taxonomic level 2 (i.e, Phylum) table'
+    )
+    def view_as_table_taxo_level_2(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.taxo_level_2_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='TaxoLevel3Table', 
+            short_description='Table view of the taxonomic level 3 (i.e, Class) table'
+    )
+    def view_as_table_taxo_level_3(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.taxo_level_3_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='TaxoLevel4Table', 
+            short_description='Table view of the taxonomic level 4 (i.e, Order) table'
+    )
+    def view_as_table_taxo_level_4(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.taxo_level_4_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='TaxoLevel5Table', 
+            short_description='Table view of the taxonomic level 5 (i.e, Family) table'
+    )
+    def view_as_table_taxo_level_5(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.taxo_level_5_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='TaxoLevel6Table', 
+            short_description='Table view of the taxonomic level 6 (i.e, Genus) table'
+    )
+    def view_as_table_taxo_level_6(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.taxo_level_6_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='TaxoLevel7Table', 
+            short_description='Table view of the taxonomic level 7 (i.e, Species) table'
+    )
+    def view_as_table_taxo_level_7(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.taxo_level_7_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+
+    @view(view_type=TableView, 
+            human_name='ObservedFeaturesVectorTable', 
+            short_description='Table view of the observed features vector index table'
+    )
+    def view_as_table_observed_features_vector(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.observed_features_vector_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='ShannonIndexTable', 
+            short_description='Table view of the shannon index table'
+    )
+    def view_as_table_shannon_vector(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.shannon_vector_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='SimpsonIndexTable', 
+            short_description='Table view of the simpson index table'
+    )
+    def view_as_table_simpson(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.simpson_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='UnweightedDistanceIndexTable', 
+            short_description='Table view of the unweighted unifrac distance table'
+    )
+    def view_as_table_unweighted_unifrac_distance(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.unweighted_unifrac_distance_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
+    @view(view_type=TableView, 
+            human_name='WeightedDistanceIndexTable', 
+            short_description='Table view of the Weighted unifrac distance table'
+    )
+    def view_as_table_weighted_unifrac_distance(self, params: ConfigParams) -> TableView:
+        table: Table = Table.import_from_path(
+            file=File(path=self.weighted_unifrac_distance_table_path),
+            params = ConfigParams({})
+        )
+
+        return TableView(data=table.get_data())
+
 
 #######################
+
+
+    # @view(view_type=TableView, 
+    #         human_name='XXXIndexTable', 
+    #         short_description='Table view of the XXX index table'
+    # )
+    # def view_as_table_observed_features(self, params: ConfigParams) -> TableView:
+    #     table: Table = Table.import_from_path(
+    #         file=File(path=self.observed_features_table_path),
+    #         params = ConfigParams({})
+    #     )
+
+    #     return TableView(data=table.get_data())  
 
 
 #    @view(view_type=TableView, 
--- src/gws_ubiome/metabarcoding/qiime2_taxonomy_diversity.py
+++ src/gws_ubiome/metabarcoding/qiime2_taxonomy_diversity.py
@@ -41,6 +41,30 @@ class Qiime2TaxonomyDiversity(Qiime2EnvTask):
         result_file = Qiime2TaxonomyDiversityFolder()
         
         result_file.path = self._output_file_path
+        
+        # Table Files
+
+        result_file.bray_curtis_table_path="/table_files/bray_curtis_distance_matrix.qza.diversity_metrics.bray_curtis_distance_matrix.qza.diversity_metrics.tsv"
+        result_file.chao_1_table_path="/table_files/chao1.qza.diversity_metrics.chao1.qza.diversity_metrics.tsv"
+        result_file.evenness_table_path="/table_files/evenness_vector.qza.diversity_metrics.evenness_vector.qza.diversity_metrics.tsv"
+        result_file.faith_pd_table_path="/table_files/faith_pd_vector.qza.diversity_metrics.faith_pd_vector.qza.diversity_metrics.tsv"
+        result_file.feature_freq_detail_table_path="/table_files/feature-frequency-detail.tsv"
+        result_file.inv_simpson_table_path="/table_files/invSimpson.tab.tsv"
+        result_file.jaccard_distance_table_path="/table_files/"
+        result_file.jaccard_unweighted_unifrac_distance_table_path="/table_files/jaccard_distance_matrix.qza.diversity_metrics.jaccard_distance_matrix.qza.diversity_metrics.tsv"
+        result_file.taxo_level_1_table_path="/table_files/level-1.tsv"
+        result_file.taxo_level_2_table_path="/table_files/level-2.tsv"
+        result_file.taxo_level_3_table_path="/table_files/level-3.tsv"
+        result_file.taxo_level_4_table_path="/table_files/level-4.tsv"
+        result_file.taxo_level_5_table_path="/table_files/level-5.tsv"
+        result_file.taxo_level_6_table_path="/table_files/level-6.tsv"
+        result_file.taxo_level_7_table_path="/table_files/level-7.tsv"
+        result_file.observed_features_vector_table_path="/table_files/observed_features_vector.qza.diversity_metrics.observed_features_vector.qza.diversity_metrics.tsv"
+        result_file.shannon_vector_table_path="/table_files/shannon_vector.qza.diversity_metrics.shannon_vector.qza.diversity_metrics.tsv"
+        result_file.simpson_table_path="/table_files/simpson.qza.diversity_metrics.simpson.qza.diversity_metrics.tsv"
+        result_file.unweighted_unifrac_distance_table_path="/table_files/unweighted_unifrac_distance_matrix.qza.diversity_metrics.unweighted_unifrac_distance_matrix.qza.diversity_metrics.tsv"
+        result_file.weighted_unifrac_distance_table_path="/table_files/weighted_unifrac_distance_matrix.qza.diversity_metrics.weighted_unifrac_distance_matrix.qza.diversity_metrics.tsv"
+
         return {"result_folder": result_file} 
     
     def build_command(self, params: ConfigParams, inputs: TaskInputs) -> list:   
