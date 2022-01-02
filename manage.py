46e1ef0d466f5519163313aae21fed29860332a3
Djomangan OUATTARA <douattara@gencovery.com>
2022-01-02 17:17:34 +0000

    (refactor)[settings] use json dict for package specs

--- manage.py
+++ manage.py
@@ -7,13 +7,15 @@ import os
 import sys
 
 if 'gws_core' not in sys.modules:
-    CORE_LIB_PATH = "/lab/user/bricks/gws_core/src"
-    if os.path.exists(CORE_LIB_PATH):
-        sys.path.insert(0, CORE_LIB_PATH)
-    else:
-        raise Exception("Cannot find the core brick")
+    CORE_LIB_PATH = "/lab/.lib/bricks/gws_core/src"
+    if not os.path.exists(CORE_LIB_PATH):
+        CORE_LIB_PATH = "/lab/user/bricks/gws_core/src"
+        if not os.path.exists(CORE_LIB_PATH):
+            raise Exception("Cannot find gws_core brick")
+    sys.path.insert(0, CORE_LIB_PATH)
 
-from gws_core import runner, manage
+from gws_core import manage, runner
+
 if __name__ == "__main__":
     __cdir__ = os.path.dirname(os.path.abspath(__file__))
     manage.load_settings(__cdir__)
