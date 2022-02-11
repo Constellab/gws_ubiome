600ccbef0b652f950b8ebabf6ede87d4d3f91785
Djomangan OUATTARA <douattara@gencovery.com>
2022-02-11 17:19:16 +0000

    (feat)[folder] convert bz2 files to gz in fastq_folder

--- .hooks/post-install.sh
+++ .hooks/post-install.sh
@@ -6,3 +6,5 @@
 
 # Post-installation script executed after module installation
 
+apt-get -y install pigz
+apt-get -y install parallel
--- src/gws_ubiome/file/fastq_folder.py
+++ src/gws_ubiome/file/fastq_folder.py
@@ -2,12 +2,41 @@
 # The use and distribution of this software is prohibited without the prior consent of Gencovery SAS.
 # About us: https://gencovery.com
 
-from gws_core import File, Folder, resource_decorator
+from typing import List, Union
+import subprocess
+from gws_core import File, Folder, resource_decorator, ShellProxy, Shell
 
 
 @resource_decorator("FastqFolder",
                     human_name="FastqFolder",
-                    short_description="FastqFolder", hide=True)
+                    short_description="FastqFolder")
 class FastqFolder(Folder):
     ''' Fastq Folder file class '''
-    pass
+    
+    
+    def check_resource(self) -> Union[str, None]:
+        """You can redefine this method to define custom logic to check this resource.
+        If there is a problem with the resource, return a string that define the error, otherwise return None
+        This method is called on output resources of a task. If there is an error returned, the task will be set to error and next proceses will not be run.
+        It is also call when uploading a resource (usually for files or folder), if there is an error returned, the resource will not be uploaded
+        """
+        
+
+        files: List[str] = self.list_dir()
+        bz2_exists = False
+        for file in files:
+            if file.endswith('.bz2'):
+                bz2_exists = True
+                break
+
+        if bz2_exists:
+            cmd = [ "for f in *.bz2 ;do bzcat < \"$f\" | pigz -9 -c >\"${f%.*}.gz\" ;done ; rm *.bz2 ;" ]
+#            cmd = [ "parallel --dry-run 'bzcat < {} | pigz -9 -c > {.}.gz' ::: *bz2 > tmp.sh ; bash tmp.sh ; rm tmp.sh ; rm *.bz2 ;" ]
+            shell_proxy = ShellProxy(Shell)
+            try:
+                shell_proxy.run(cmd, cwd=self.path, shell_mode=True)
+                return None
+            except Exception as err:
+                return f"Cannot upload the folder. Error message: {err}"
+        else:
+            return None
