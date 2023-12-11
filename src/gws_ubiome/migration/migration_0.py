2eb2903e5295db23b08b83c87f7e95f7b6a06be3
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2023-12-11 13:26:08 +0000

    (fix) fix migration of typing names

--- src/gws_ubiome/migration/migration_0.py
+++ src/gws_ubiome/migration/migration_0.py
@@ -3,8 +3,11 @@
 # The use and distribution of this software is prohibited without the prior consent of Gencovery SAS.
 # About us: https://gencovery.com
 
-from gws_core import (BrickMigration, File, Folder, ResourceModel, Version,
-                      brick_migration)
+from json import dumps, loads
+from typing import List
+
+from gws_core import (BrickMigration, File, Folder, ResourceModel, TaskModel,
+                      Version, brick_migration)
 from gws_omix import FastqFolder
 
 
@@ -14,24 +17,24 @@ class Migration060(BrickMigration):
     @classmethod
     def migrate(cls, from_version: Version, to_version: Version) -> None:
 
-        # Folders
-        ResourceModel.replace_resource_typing_name(
-            'RESOURCE.gws_ubiome.Qiime2TaxonomyDiversityFolder', Folder._typing_name)
-        ResourceModel.replace_resource_typing_name(
-            'RESOURCE.gws_ubiome.Qiime2FeatureFrequencyFolder', Folder._typing_name)
-        ResourceModel.replace_resource_typing_name(
-            'RESOURCE.gws_ubiome.Qiime2DifferentialAnalysisResultFolder', Folder._typing_name)
-        ResourceModel.replace_resource_typing_name(
-            'RESOURCE.gws_ubiome.Qiime2QualityCheckResultFolder', Folder._typing_name)
-        ResourceModel.replace_resource_typing_name(
-            'RESOURCE.gws_ubiome.Qiime2RarefactionAnalysisResultFolder', Folder._typing_name)
+        typing_dict = {
+            'RESOURCE.gws_ubiome.Qiime2TaxonomyDiversityFolder': Folder._typing_name,
+            'RESOURCE.gws_ubiome.Qiime2FeatureFrequencyFolder': Folder._typing_name,
+            'RESOURCE.gws_ubiome.Qiime2DifferentialAnalysisResultFolder': Folder._typing_name,
+            'RESOURCE.gws_ubiome.Qiime2QualityCheckResultFolder': Folder._typing_name,
+            'RESOURCE.gws_ubiome.Qiime2RarefactionAnalysisResultFolder': Folder._typing_name,
+            'RESOURCE.gws_ubiome.Qiime2ManifestTableFile': File._typing_name,
+            'RESOURCE.gws_ubiome.Qiime2MetadataTableFile': File._typing_name,
+            'RESOURCE.gws_ubiome.FastqFolder': FastqFolder._typing_name
+        }
 
-        # Files
-        ResourceModel.replace_resource_typing_name(
-            'RESOURCE.gws_ubiome.Qiime2ManifestTableFile', File._typing_name)
-        ResourceModel.replace_resource_typing_name(
-            'RESOURCE.gws_ubiome.Qiime2MetadataTableFile', File._typing_name)
+        for key, value in typing_dict.items():
+            ResourceModel.replace_resource_typing_name(key, value)
 
-        # FastqFolder
-        ResourceModel.replace_resource_typing_name(
-            'RESOURCE.gws_ubiome.FastqFolder', FastqFolder._typing_name)
+        # replace the references in the tasks inputs and outputs specs
+        tasks_models: List[TaskModel] = list(TaskModel.select())
+        for task_model in tasks_models:
+            for key, value in typing_dict.items():
+                task_model.data = loads(dumps(task_model.data).replace(key, value))
+
+            task_model.save(skip_hook=True)
