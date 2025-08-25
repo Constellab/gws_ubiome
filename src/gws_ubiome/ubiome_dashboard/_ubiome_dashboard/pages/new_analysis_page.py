c197ed722db2073bc79436c4cabeaf476d7a97d7
Maëva <mbeugin@gencovery.com>
2025-08-25 08:49:26 +0000

    Add mandatory fields check

--- src/gws_ubiome/ubiome_dashboard/_ubiome_dashboard/pages/new_analysis_page.py
+++ src/gws_ubiome/ubiome_dashboard/_ubiome_dashboard/pages/new_analysis_page.py
@@ -3,7 +3,7 @@ from state import State
 from gws_core.streamlit import StreamlitContainers, StreamlitResourceSelect, StreamlitRouter, StreamlitTaskRunner
 from gws_ubiome.ubiome_dashboard._ubiome_dashboard.ubiome_config import UbiomeConfig
 import pandas as pd
-from gws_core import SpaceFolder, StringHelper, Tag, InputTask, SpaceService, ProcessProxy, ScenarioSearchBuilder, TagValueModel, Scenario, ScenarioStatus, ScenarioProxy, ProtocolProxy, ScenarioCreationType
+from gws_core import ResourceModel, SpaceFolder, StringHelper, Tag, InputTask, SpaceService, ProcessProxy, ScenarioSearchBuilder, TagValueModel, Scenario, ScenarioStatus, ScenarioProxy, ProtocolProxy, ScenarioCreationType
 from gws_ubiome import Qiime2MetadataTableMaker
 
 def render_new_analysis_page():
@@ -20,14 +20,14 @@ def render_new_analysis_page():
         st.markdown("## New Analysis")
         # select fastq data
         resource_select = StreamlitResourceSelect()
-        selected_fastq = resource_select.select_resource(
-            placeholder='Search for fastq resource', key="resource-selector", defaut_resource=None)
+        resource_select.select_resource(
+            placeholder='Search for fastq resource', key=ubiome_state.RESOURCE_SELECTOR_FASTQ_KEY, defaut_resource=None)
 
         form_config = StreamlitTaskRunner(Qiime2MetadataTableMaker)
         form_config.generate_config_form_without_run(
             session_state_key=ubiome_state.QIIME2_METADATA_CONFIG_KEY, default_config_values=Qiime2MetadataTableMaker.config_specs.get_default_values())
 
-        analysis_name = st.text_input("Insert your analysis name")
+        st.text_input("Insert your analysis name", key = ubiome_state.ANALYSIS_NAME_USER)
 
         space_service = SpaceService.get_instance()
         list_folders_in_lab = space_service.get_all_lab_root_folders().folders
@@ -46,6 +46,17 @@ def render_new_analysis_page():
         )
 
         if submit_button:
+            list_required_fields_filled = []
+            list_required_fields_filled.append(ubiome_state.check_if_required_is_filled(ubiome_state.get_resource_selector_fastq()))
+            list_required_fields_filled.append(ubiome_state.check_if_required_is_filled(ubiome_state.get_analysis_name_user()))
+            # Check if mandatory fields have been filled
+            if False in list_required_fields_filled:
+                st.warning("Please fill all the mandatory fields: fastq resource and analysis name.")
+                return
+
+            selected_fastq_id = ubiome_state.get_resource_selector_fastq()["resourceId"]
+            selected_fastq = ResourceModel.get_by_id(selected_fastq_id)
+            analysis_name = ubiome_state.get_analysis_name_user()
             # Create a new scenario in the lab
             folder : SpaceFolder = SpaceFolder.get_by_id(ubiome_state.get_selected_folder_id())
             scenario: ScenarioProxy = ScenarioProxy(
